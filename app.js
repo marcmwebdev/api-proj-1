3b7d688437b82d0d25b3dc61d06b6553106d82c6
Marc M <marcmwebdev@gmail.com>
2021-12-30 01:04:24 -0500

    first

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 1337;
+// const { getRestName } = require('./sandData')
+
+app.use(express.json())
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/restname', (req, res) => {
+  res.render('restname', {rest1: 'Sam Lagrassa\'s', rest2: 'Bob\s Italian Foods', rest3: 'Eagle Deli'});
+});
+
+app.get('/restname/:param', (req, res) => {
+  res.render('sand', {sand1: 'Combo Reuben', sand2: 'Imported Italian', sand3: 'Some Sandwich'});
+});
+
+// app.get('/group/:restaurants', (req, res) => {
+//   res.render('group', { getRestName })
+// })
+
+app.get('/suggestion', (req, res) => {
+  res.render('suggestion');
+});
+
+app.post('/suggestion', (req, res) => {
+res.render('suggestion')
+res.status(201).send('Added Suggestion');
+});
+
+
+app.listen(port, () => {
+console.log('listening on port ${port}');
+});
---
+++ sanddata.js
@@ -0,0 +1,19 @@
+const sandData = [{
+  group: 'Sandwich Restaurants',
+  restaurants:[{ 
+    restname: 'Sam Lagrassa\'s',
+    address: 'Address: 44 Province St, Boston, MA 02108',
+    bestSand: 'Combo Reuben'
+  },{ 
+    restname: 'Bob\'s Italian Foods',
+    address: 'Address: 324 Main St, Medford, MA 02155',
+    bestSand: 'Imported Italian'
+  },{
+    restname: 'Eagle Deli',
+    address: 'Address: 1918 Beacon St, Brighton, MA 02135',
+    bestSand: 'Some Sandwich'
+  }]  
+
+}]
+
+module.exports = sandData
